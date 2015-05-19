628869f6c58084a34f5d00fd3ad50f2b2bbffcec
mamamun <almamun@isrt.ac.bd>
2015-05-19 00:24:07 -0500

    message

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  
+  setmean <- function(mean) {
+    m <<- mean
+  }
+  getmean <- function() {
+    m
+  }
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmean(m)
+  m
 }
+
+# vec<- makeCacheMatrix()
+# samplevector<- matrix(rnorm(5*5),5,5)
+# vec$set(samplevector)
+# vec$get()
+# cacheSolve(vec)
+# cacheSolve(vec)
