80f2207e21a7d5d15cf9980d259c463ac9ebadd4
mamamun <almamun@isrt.ac.bd>
2015-05-19 01:02:57 -0500

    message

--- cachematrix.R
+++ cachematrix.R
@@ -10,31 +10,33 @@ makeCacheMatrix <- function(x = matrix()) {
   
   setSolve <- function(Solve) {
     m <<- Solve               # Lexical scoping. So x will be accessible beyond this "setSolve" function
-  }
+  }                           # Repository for the inverse matrix
   getSolve <- function() {
-    m
+    m                         # Repository for the inverse matrix
   }
-  list(set = set, get = get,
+  list(set = set, get = get,  # Return list objects
        setSolve = setSolve,
        getSolve = getSolve)
 }
 
 
 cacheSolve <- function(x, ...) {
-  m <- x$getSolve()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
+  m <- x$getSolve()           # Getting the inverse matrix from cache
+  
+  if(is.null(m)) {           # It will be a NA if there is no inverse matrix in the cache 
+    data <- x$get()             # Getting the input matrix
+    m <- solve(data, ...)       # solve a base function to inverse the input matrix
+    x$setSolve(m)               # put the inverse matrix in the cache
   }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setSolve(m)
-  m
+  else {
+    message("getting cached data")   # If already inverse was stored in the cache.
+  }
+  return(m)                           # Return inverse matrix
 }
 
-vec<- makeCacheMatrix()
-samplevector<- matrix(rnorm(5*5),5,5)
-vec$set(samplevector)
-vec$get()
-cacheSolve(vec)
-cacheSolve(vec)
+# vec<- makeCacheMatrix()
+# samplevector<- matrix(rnorm(3*3),3,3)
+# vec$set(samplevector)
+# vec$get()
+# cacheSolve(vec)
+# cacheSolve(vec)
